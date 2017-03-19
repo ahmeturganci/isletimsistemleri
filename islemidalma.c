f8b4d7c4b517acfb2fb17a9a3c560657d2a368f1
ahmeturganci <aahmet.272@gmail.com>
2017-03-19 20:35:34 +0200

    sleep();

--- islemidalma.c
+++ islemidalma.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 int main(int argc, char const *argv[])
 {
-	printf("İşlem/süreç id: %d\n",getpid());
-	printf("Ebevey işlem/süreç id:%d\n",getppid());
+	printf("İşlem/süreç id: %d\n",getpid());//işlem/süreç id getirmek.
+	printf("Ebevey işlem/süreç id:%d\n",getppid());// ebeveyb id getirmek.
+	sleep(30);//30sn sonra işlem/süreç sonlanacak.
 	return 0;
 }
